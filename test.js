3a20928ad14efa66fce37b51b5ce8c7674686716
mig1714 <63379958+mig1714@users.noreply.github.com>
2026-06-17 01:09:34 +0000

    feat: Se crea la prueba con jest

--- test.js
+++ test.js
@@ -32,20 +32,54 @@ test("One euro should be 1.07 dollars", function() {
 
 test("One euro should be 1.07 dollars", function() {
     // Import the function from app.js
-    const { fromDollarToYen } = require('./app.js');
+    const { fromEuroToDollar } = require('./app.js');
 
     // Use the function like its supposed to be used
-    const dollars = fromDollarToYen(10);
+    const dollars = fromEuroToDollar(3.5);
 
-    // If 1 YEN is 0.0062 dollars, then 10 YEN should be (10 * 0.0062)
-    const expected = 10 * 9.345794392523365; 
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = 3.5 * 1.07; 
     
     // This is the comparison for the unit test
-     expect(fromDollarToYen(10)).toBe(1462.6168224299065); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
 
 
-test("One yen should be 156.5 yens", function() {
+test("One euro deberia ser un numero", function() {
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const resultado = fromDollarToYen(10);
+
+    // If 1 YEN is 0.0062 dollars, then 10 YEN should be (10 * 0.0062)
+    const expected = 10; 
+    
+    // This is the comparison for the unit test
+    expect(typeof resultado).toBe('number');
+    expect(Number.isNaN(resultado)).toBe(false);
+})
+
+test("cero dolares deberia ser cero", function() {
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const resultado = fromDollarToYen(0);
+
+    // If 1 YEN is 0.0062 dollars, then 10 YEN should be (10 * 0.0062)
+    
+    
+    // This is the comparison for the unit test
+    expect(resultado).toBe(0);
+    
+})
+
+
+
+
+
+test("One yen should be 156.5 a libras", function() {
     // Import the function from app.js
     const { fromYenToPound } = require('./app.js');
 
@@ -57,4 +91,38 @@ test("One yen should be 156.5 yens", function() {
     
     // This is the comparison for the unit test
      expect(fromYenToPound(10)).toBe(8.7); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
+
+
+
+
+
+test("One yen should be 156.5 libras", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const yens = fromYenToPound(10);
+
+    // If 1 YEN is 0.0062 dollars, then 10 YEN should be (10 * 0.87)
+    const resultado = 10 * 0.87; 
+    
+    // This is the comparison for the unit test
+    expect(typeof resultado).toBe("number");// deberia ser un numero
+    expect(Number.isNaN(resultado)).toBe(false);
+})
+
+test("cero yenes deberia ser cero", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const resultado = fromYenToPound(0);
+
+    // If 1 YEN is 0.0062 dollars, then 10 YEN should be (10 * 0.0062)
+    
+    
+    // This is the comparison for the unit test
+    expect(resultado).toBe(0);
+    
 })
